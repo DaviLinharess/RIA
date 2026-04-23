c7ce7e4d4cb1bb2f21c2772d5c4cd7cbde7c9e7c
Davi Linhares <davimedeiroslinhares14@gmail.com>
2026-04-23 19:10:21 +0000

    feat: CRUD de perfumes finalizado com PrimeNG 21

--- CRUD/src/app/app.ts
+++ CRUD/src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';               //loops e condicionais
 import { FormsModule } from '@angular/forms';                 // forms (inputs)
-import { RouterOutlet } from '@angular/router';
 
 //Imports do PrimeNG
 import { ButtonModule } from 'primeng/button';
@@ -23,7 +22,6 @@ interface Item {
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     CommonModule,
     FormsModule,
     ButtonModule,
@@ -51,11 +49,19 @@ export class App {
     if (this.novoItem.nome) {
       const listaAtual = this.itens();
 
-      const novoId = listaAtual.length > 0                  // se a lista estiver vazia, o ID é 1
-      ? Math.max(...listaAtual.map(item => item.id)) + 1    // se não, busca o maior ID existente e soma +1
+      if (this.novoItem.id > 0) { // Se o ID for maior que 0, é um item existente
+
+        //  Atualiza o item existente, substituindo o item antigo pelo novo na lista
+        this.itens.update(lista => lista.map(i => i.id === this.novoItem.id ? { ...this.novoItem } : i));
+
+      } else {
+        // Gerar um novo ID para o item
+        const novoId = listaAtual.length > 0                  // se a lista estiver vazia, o ID é 1
+      ? Math.max(...listaAtual.map(item => item.id)) + 1      // se não, busca o maior ID existente e soma +1
       : 1;
-                                                            // atualiza o Signal criando uma nova lista
+                                                              // atualiza o Signal criando uma nova lista
       this.itens.update(lista => [...lista, {...this.novoItem, id: novoId }]);
+      }
 
       this.resetForm();
     }
@@ -68,8 +74,12 @@ export class App {
     this.itens.update(lista => lista.filter(item => item.id !== id));
   }
 
-  private resetForm() {
+  resetForm() {
     this.novoItem = { id: 0, nome: '', valor: 0, ativo: true };
+  }
+
+  prepararEdicao(item: Item) {
+    this.novoItem = {...item};                              // Clona o item para evitar complicações diretas
   }
 
   protected readonly title = signal('CRUD');
